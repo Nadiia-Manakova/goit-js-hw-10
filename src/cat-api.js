02ce5560688ca5c8c56030f14772f890de0aeee9
Nadiia-Manakova <nadiiamanakova33@gmail.com>
2023-10-25 20:15:58 +0200

    start task

---
+++ src/cat-api.js
@@ -0,0 +1,13 @@
+fetchBreeds ("https://api.thecatapi.com/v1/breeds")
+ .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Data handling
+  })
+  .catch(error => {
+    // Error handling
+  });
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+// import SlimSelect from 'slim-select'
+// import Notiflix from 'notiflix';
+
+axios.defaults.headers.common["x-api-key"] = "live_pLFsKr3BoHrT6i9dasqBQMO6tOhQcD76K2Vhyy0hO26KVmnqZHxcrfVnIPjU7tfX";
+
+// new SlimSelect({
+//   select: '.breed-select'
+// })
+
+// Notiflix.Notify.info('Oops! Something went wrong! Try reloading the page!');
